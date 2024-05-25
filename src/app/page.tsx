40e15b889aaa56bc9baf0d9c2780c0ebc24c628b
Daniil Batiuk <sportak50@gmail.com>
2024-05-25 17:03:47 +0300

    🧨 fix bug with full route cache

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ import { useRouter } from "next/navigation";
 
 import styles from "./page.module.scss";
 
+export const dynamic = "force-dynamic";
+
 export default function Home() {
   const router = useRouter();
 
