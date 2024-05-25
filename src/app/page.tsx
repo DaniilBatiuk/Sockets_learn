8209166179dc500b986e2cb8852004cac7ea93e8
Daniil Batiuk <sportak50@gmail.com>
2024-05-25 15:10:54 +0300

    🧨 fix bug with cache

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,16 @@
 "use client";
 
-import { Button, TextField } from "@mui/material";
+import { Button } from "@mui/material";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
 
 import styles from "./page.module.scss";
 
 export default function Home() {
-  const [roomId, setRoomId] = useState("");
   const router = useRouter();
 
   const createRoom = async () => {
-    const res = await fetch("/api/rooms/create");
+    const res = await fetch("/api/rooms/create", { cache: "no-store" });
     const roomId: string = await res.text();
-    router.push(`/room/${roomId}`);
-  };
-
-  const joinRoom = async () => {
     router.push(`/room/${roomId}`);
   };
 
@@ -25,22 +19,6 @@ export default function Home() {
       <Button variant="contained" onClick={createRoom} color="success">
         Create room
       </Button>
-      <div className={styles.home__main}>
-        <TextField
-          sx={{ width: 210 }}
-          label="Input room id"
-          type="search"
-          variant="standard"
-          value={roomId}
-          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-            setRoomId(event.target.value);
-          }}
-        />
-
-        <Button variant="contained" onClick={joinRoom}>
-          Join room
-        </Button>
-      </div>
     </div>
   );
 }
