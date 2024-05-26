41a8bc16dd17a298d6cc08a0802ffadf31f7f1e7
Daniil Batiuk <sportak50@gmail.com>
2024-05-26 10:12:44 +0300

    🚩add loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import { Button } from "@mui/material";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 import styles from "./page.module.scss";
 
@@ -9,17 +10,20 @@ export const dynamic = "force-dynamic";
 
 export default function Home() {
   const router = useRouter();
+  const [loading, setLoading] = useState(false);
 
   const createRoom = async () => {
+    setLoading(true);
     const res = await fetch("/api/rooms/create", { cache: "no-store" });
     const roomId: string = await res.text();
     router.push(`/room/${roomId}`);
+    setLoading(false);
   };
 
   return (
     <div className={styles.home__container}>
-      <Button variant="contained" onClick={createRoom} color="success">
-        Create room
+      <Button variant="contained" onClick={createRoom} color="success" disabled={loading}>
+        {!loading ? "Create room" : "Loading..."}
       </Button>
     </div>
   );
